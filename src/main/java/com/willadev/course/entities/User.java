5ef8fee129f015d252642983178bc42e21dead2b
willa <willaevangelista.pro@gmail.com>
2026-07-26 18:53:52 -0300

    Order, Instant, ISO 8601 - PART 1

--- src/main/java/com/willadev/course/entities/User.java
+++ src/main/java/com/willadev/course/entities/User.java
@@ -1,9 +1,16 @@
 package com.willadev.course.entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.OneToMany;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -20,6 +27,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User () {}
 
@@ -69,6 +79,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
---
+++ src/main/java/com/willadev/course/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.willadev.course.enums;
+
+public enum OrderStatus {
+    WAITING_PAYMENT,
+    PAID,
+    SHIPPED,
+    DELIVERED,
+    CANCELED
+}
