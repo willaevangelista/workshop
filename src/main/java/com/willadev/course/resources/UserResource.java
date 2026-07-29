c3df67899af14bd30609ec601c65b514c0b41181
willa <willaevangelista.pro@gmail.com>
2026-07-29 18:49:49 -0300

    User delete

--- src/main/java/com/willadev/course/resources/UserResource.java
+++ src/main/java/com/willadev/course/resources/UserResource.java
@@ -36,4 +36,10 @@ public class UserResource {
                     .buildAndExpand(user.getId()).toUri();
         return ResponseEntity.created(uri).body(user);
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
