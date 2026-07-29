824fc6a8fedb1217734d110f31319a2a9bbc7635
willa <willaevangelista.pro@gmail.com>
2026-07-29 18:07:39 -0300

    Payment, one-to-one association

--- src/main/java/com/willadev/course/entities/Order.java
+++ src/main/java/com/willadev/course/entities/Order.java
@@ -24,7 +24,6 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
-
     private Integer orderStatus;
 
     @ManyToOne
@@ -33,6 +32,9 @@ public class Order implements Serializable {
 
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
+
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
 
     public Order() {}
 
@@ -76,6 +78,14 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     public Set<OrderItem> getItems() {
