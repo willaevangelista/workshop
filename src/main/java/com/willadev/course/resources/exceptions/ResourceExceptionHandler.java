1693f5dbf6391d6556b57a7a8231d396d5a711f0
willa <willaevangelista.pro@gmail.com>
2026-08-01 15:29:14 -0300

    Exception handling - delete

--- src/main/java/com/willadev/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/willadev/course/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.willadev.course.resources.exceptions;
 
+import com.willadev.course.services.exceptions.DatabaseException;
 import com.willadev.course.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -19,6 +20,14 @@ public class ResourceExceptionHandler {
         HttpStatus status = HttpStatus.NOT_FOUND;
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
 }
--- src/main/java/com/willadev/course/services/UserService.java
+++ src/main/java/com/willadev/course/services/UserService.java
@@ -2,8 +2,11 @@ package com.willadev.course.services;
 
 import com.willadev.course.entities.User;
 import com.willadev.course.repositories.UserRepository;
+import com.willadev.course.services.exceptions.DatabaseException;
 import com.willadev.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -41,6 +44,12 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        userRepository.deleteById(id);
+        try {
+            userRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 }
