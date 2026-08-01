61fabbabfc4cbea1758816c4ceb8a250f813c439
willa <willaevangelista.pro@gmail.com>
2026-08-01 15:38:15 -0300

    Exception handling - update

--- src/main/java/com/willadev/course/services/UserService.java
+++ src/main/java/com/willadev/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.willadev.course.entities.User;
 import com.willadev.course.repositories.UserRepository;
 import com.willadev.course.services.exceptions.DatabaseException;
 import com.willadev.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -32,9 +33,13 @@ public class UserService {
     }
 
     public User update(Long id, User user) {
-        User entity = userRepository.getReferenceById(id);
-        updateData(entity, user);
-        return userRepository.save(entity);
+        try {
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, user);
+            return userRepository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User user) {
